9cade66da09ad616dab74a21c3c8531fd6148818
Alexey Alekhin <laughedelic@gmail.com>
2013-05-13 07:01:10 +0200

    Removed hardcoded gtm location. Should work if sublime is run from shell. On OS X sublime has strange PATH when run from Finder.

--- sublime-gtm.py
+++ sublime-gtm.py
@@ -1,21 +1,26 @@
 import sublime, sublime_plugin
 import subprocess
 import os.path
+import os
+import re
+
+def strip_ansi_codes(s):
+	return re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
 
 def status(view):
 	filename = view.file_name()
 	if filename:
 		dir = os.path.dirname(filename)
-		# gtm_p = subprocess.Popen(['gtm', 'status', '--short', '--no-label']
-		# gtm_p = subprocess.Popen('/usr/local/bin/gtm prompt-status'
-		gtm_p = subprocess.Popen('/usr/local/bin/gtm status -s'
+		env = os.environ
+		gtm_p = subprocess.Popen('gtm status -s'
 			, cwd=dir
+			, env=env
 			, shell=True
 			, stdout=subprocess.PIPE )
 		exit_code = gtm_p.wait()
 		# print(exit_code)
 		if exit_code == 0: 
-			out = gtm_p.communicate()[0].decode("utf-8") 
+			out = strip_ansi_codes(gtm_p.communicate()[0].decode("utf-8"))
 			status = out.replace('[on]','  [ON]').replace('[off]','  [OFF]')
 			view.set_status('gtm', 'gtm: ' + status)
 		else: view.erase_status('gtm')
@@ -29,5 +34,3 @@ class GtmListener(sublime_plugin.EventListener):
 		status(view)
 	def on_activated_async(self, view):
 		status(view)
-	# def on_deactivated_async(self, view):
-	# 	status(view)
