cc98067350ed5fc7d32d0c19492de8456ab510b8
Alexey Alekhin <laughedelic@gmail.com>
2013-05-16 01:49:32 +0200

    Added check for gtm availability

--- sublime-gtm.py
+++ sublime-gtm.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
 import subprocess
-import os, os.path, re
+import os, os.path, re, sys
 
 def strip_ansi_codes(s):
 	return re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
@@ -19,10 +19,17 @@ def call_gtm(view, cmd):
 		# print(exit_code)
 		if exit_code == 0: 
 			return strip_ansi_codes(gtm_p.communicate()[0].decode("utf-8"))
-		else: return None
+		else: 
+			if exit_code == 127: 
+				raise Exception("gtm is not found in Sublime's PATH: " + env["PATH"])
+			return None
 
 def status(view):
-	stat = call_gtm(view, 'status --short')
+	stat = None
+	try:
+		stat = call_gtm(view, 'status --short')
+	except Exception as err:
+		print('gtm error: ', err)
 	if stat == None: view.erase_status('gtm')
 	else: 
 		status = stat.replace('[on]','  [ON]').replace('[off]','  [OFF]')
@@ -41,4 +48,21 @@ class GtmListener(sublime_plugin.EventListener):
 class GtmCommand(sublime_plugin.WindowCommand):
 	def run(self, cmd):
 		print('gtm ' + cmd + ':')
-		print(call_gtm(self.window.active_view(), cmd))
+		try:
+			print(call_gtm(self.window.active_view(), cmd))
+		except Exception as err:
+			sublime.error_message(str(err))
+
+# class GtmListCommand(sublime_plugin.WindowCommand):
+# 	def run(self):
+# 		v = self.window.active_view()
+# 		tasks = call_gtm(v, 'list --no-header')
+# 		print(tasks)
+# 		def switch_task(i):
+# 			if i == -1: status(v)
+# 			return
+# 		# for t in tasks.splitlines(): 
+# 			# t.split(' ')
+# 		ts = [re.sub(r'\s+', ' ', t) for t in tasks.splitlines()]
+# 		print(ts)
+# 		self.window.show_quick_panel(ts, switch_task, sublime.MONOSPACE_FONT)
